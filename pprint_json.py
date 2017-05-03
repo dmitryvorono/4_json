43ac0b3cc6a4cfed26af1bf131d4ae10cf113647
dvoronov <voronov-d88@yandex.ru>
2017-05-03 07:27:56 +0300

    Add

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
 import json
+import sys
+import os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print('Usage: python pprint_json.py <path to file>')
+        sys.exit(2)
+    pretty_print_json(load_data(sys.argv[1]))
